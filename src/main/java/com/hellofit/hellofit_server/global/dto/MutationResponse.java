326ee578a7340d44f7d0876cac31c2ac066daba8
yunjongyun <yunyami0605@gmail.com>
2025-08-21 00:17:49 +0900

    fix: 공통 응답 dto 수정
    1. builder 추가

--- src/main/java/com/hellofit/hellofit_server/global/dto/MutationResponse.java
+++ src/main/java/com/hellofit/hellofit_server/global/dto/MutationResponse.java
@@ -1,11 +1,13 @@
 package com.hellofit.hellofit_server.global.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class MutationResponse {
     private UUID id;
 }
