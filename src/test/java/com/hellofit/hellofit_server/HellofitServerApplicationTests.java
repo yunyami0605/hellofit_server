470cf3d564d90873e4833d51fc3ca23fb5d8eb8b
yunjongyun <yunyami0605@gmail.com>
2025-09-22 11:31:05 +0900

    chore: test 설정 파일 추가
    1. test 설정 파일 추가

--- src/test/java/com/hellofit/hellofit_server/HellofitServerApplicationTests.java
+++ src/test/java/com/hellofit/hellofit_server/HellofitServerApplicationTests.java
@@ -2,12 +2,14 @@ package com.hellofit.hellofit_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HellofitServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
